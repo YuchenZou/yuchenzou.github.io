c1eb826cf3753bdf50a5b5aaddc5ce91cdbc42d4
Yuchen Zou <yuchenzou@stu.xjtu.edu.cn>
2026-07-27 16:04:50 +0800

    Update publication links and author marks

--- site-data.js
+++ site-data.js
@@ -85,7 +85,7 @@ const siteData = {
   publications: [
     {
       title: "CityPaint: A Free Lunch from 2D Diffusion for City-Scale 3D Texturing",
-      authors: '<a href="https://openreview.net/profile?id=~Yuchen_Zou2" target="_blank" rel="noreferrer"><strong>Yuchen Zou<sup>†</sup></strong></a>, <a href="https://openreview.net/profile?id=~Lihuang_Fang2" target="_blank" rel="noreferrer">Lihuang Fang<sup>†</sup></a>, <a href="https://openreview.net/profile?id=~Siyu_Zhu8" target="_blank" rel="noreferrer">Siyu Zhu</a>, <a href="https://openreview.net/profile?id=~Xiao_Hu13" target="_blank" rel="noreferrer">Xiao Hu</a> (<sup>†</sup> Equal contribution)',
+      authors: '<a href="https://openreview.net/profile?id=~Yuchen_Zou2" target="_blank" rel="noreferrer"><strong>Yuchen Zou<sup>†</sup></strong></a>, <a href="https://openreview.net/profile?id=~Lihuang_Fang2" target="_blank" rel="noreferrer">Lihuang Fang<sup>†</sup></a>, <a href="https://openreview.net/profile?id=~Siyu_Zhu8" target="_blank" rel="noreferrer">Siyu Zhu</a>, <a href="https://openreview.net/profile?id=~Xiao_Hu13" target="_blank" rel="noreferrer">Xiao Hu*</a> (<sup>†</sup> Equal contribution)',
       venue: "ACM MM 2026",
       image: "./assets/images/publications/acmmm26_citypaint.png",
       links: [],
@@ -116,6 +116,7 @@ const siteData = {
       links: [
         { label: "Project", href: "https://yuchenzou.github.io/FlowPalm/" },
         { label: "Paper", href: "http://arxiv.org/abs/2604.09989" },
+        { label: "Code", href: "https://github.com/YuchenZou/FlowPalm-master" },
       ],
     },
     {
